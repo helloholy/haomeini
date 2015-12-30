4ce63903082b5e92a40846da130147a8a6e2545f
tarena <tarena@fire-hao.local>
2015-12-30 19:39:01 +0800

    111

---
+++ yi/yi/ddxdd.h
@@ -0,0 +1,13 @@
+//
+//  ddxdd.h
+//  yi
+//
+//  Created by tarena on 15/12/30.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ddxdd : NSObject
+
+@end
---
+++ yi/yi/ddxdd.m
@@ -0,0 +1,13 @@
+//
+//  ddxdd.m
+//  yi
+//
+//  Created by tarena on 15/12/30.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import "ddxdd.h"
+
+@implementation ddxdd
+
+@end
